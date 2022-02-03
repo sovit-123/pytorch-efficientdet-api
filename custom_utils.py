5cdef5e3735bcf4626f806ca00d105029e481537
sovit-123 <sovitrath5@gmail.com>
2022-02-03 10:14:51 +0530

    Custom Utils changes

--- custom_utils.py
+++ custom_utils.py
@@ -62,7 +62,7 @@ def collate_fn(batch):
     To handle the data loading as different images may have different number 
     of objects and to handle varying size tensors as well.
     """
-    batch = list(filter(lambda x: x is not None, batch))
+    # batch = list(filter(lambda x: x is not None, batch))
     return tuple(zip(*batch))
 
 # define the training tranforms
